c720b8fae519d6ac07066ff79e6c3bd4498efac9
Tassilo Schneidt <tschneidt@gmx.de>
2022-07-20 22:17:59 +0200

    Created first UI

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import sys
+from qtpy import QtWidgets
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from ui.mainwindow import Ui_MainWindow
 
+app = QtWidgets.QApplication(sys.argv)
+window = QtWidgets.QMainWindow()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+ui_window = Ui_MainWindow()
+ui_window.setupUi(window)
+window.show()
+sys.exit(app.exec_())
