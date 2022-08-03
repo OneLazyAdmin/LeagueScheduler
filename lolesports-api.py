509d05b48aca42a8d6eac0ff3e6209db34e5af60
Tassilo Schneidt <tschneidt@gmx.de>
2022-08-03 21:18:22 +0200

    Improved the request + saving of the result. Made it a function Improved the GUI

--- lolesports-api.py
+++ lolesports-api.py
@@ -2,49 +2,60 @@
 
 import requests
 import json
+
+# non-changing variables
 apikey = '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z'
 
-url = "https://esports-api.lolesports.com/persisted/gw/getSchedule?hl=en-GB&leagueId=98767991302996019"
 
-payload={}
-headers = {'hl': 'en-US', 'x-api-key': '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z'}
+# print(schedule_pretty)
 
-response = (requests.get(url, headers=headers, data=payload))
-data = response.json()
-schedule_pretty = json.dumps(data, indent=4)
-schedule = json.dumps(data)
+# with open('./schedule.json', 'w') as f:
+#    json.dump(schedule_file, f)
 
-print(schedule_pretty)
-
-with open('./schedule.json', 'w') as f:
-   json.dump(schedule, f)
+# with open('./schedule_pretty.json', 'w') as f:
+#    json.dump(schedule_pretty.strip(), f)
+# print(schedule_pretty)
 
 
-with open('./schedule_pretty.json', 'w') as f:
-   json.dump(schedule_pretty.strip(), f)
-print(schedule_pretty)
-myEvents = data["data"]["schedule"]["events"]
-print(myEvents)
-#How to get the data
-#for n_event in myEvents:
+# print(myEvents)
+# How to get the data
+# for n_event in myEvents:
 #    print(n_event["blockName"])
-for n_event in myEvents:
-    #if n_event["blockName"] =='Week 6':
-        print(n_event["blockName"])
-        print(n_event["match"]["teams"])
+
+def get_schedule():
+    url = "https://esports-api.lolesports.com/persisted/gw/getSchedule?hl=en-GB&leagueId=98767991302996019"
+
+    payload = {}
+    headers = {'hl': 'en-US', 'x-api-key': '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z'}
+
+    response = (requests.get(url, headers=headers, data=payload))
+    data = response.json()
+    schedule_pretty = json.dumps(data, indent=4)
+    schedule_file = json.dumps(data)
+    print(data)
+    my_events = data["data"]["schedule"]["events"]
+    schedule = []
+    for n_event in my_events:
+        # if n_event["blockName"] =='Week 6':
+        start_time = n_event["startTime"].split("T")[1].split("Z")[0]
+        week = n_event["blockName"].split(" ")[1]
+        print(week)
+        # print(n_event["match"]["teams"])
         team1 = n_event["match"]["teams"][0]["name"]
         team2 = n_event["match"]["teams"][1]["name"]
         result = n_event["match"]["teams"][0]["result"]["outcome"]
         if result == "win":
-            winner = "Winner: " + team1
+            winner = team1
         elif result == "loss":
-            winner = "Winner: " + team2
-        elif result == None:
-            winner = "Winner: TBD"
-        print(team1 + " vs. " + team2)
+            winner = team2
+        elif result is None:
+            winner = "TBD"
+        match = team1 + " vs. " + team2
+        # print(team1 + " vs. " + team2)
         print(winner)
+        temp = {"week": week, "match": match, "winner": winner, "startTime": start_time}
+        schedule.append(temp)
+    print(schedule)
 
 
-
-
-
+get_schedule()
--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import sys
 from qtpy import QtWidgets
-
 from ui.mainwindow import Ui_MainWindow
 
 app = QtWidgets.QApplication(sys.argv)
-window = QtWidgets.QMainWindow()
 
-ui_window = Ui_MainWindow()
-ui_window.setupUi(window)
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.get_schedule.clicked.connect(self.onPushButtonClick)
+        self.ui.tableWidget.cellChanged.connect(self.onCellChanged)
+
+    def onPushButtonClick(self):
+        row = self.ui.tableWidget.rowCount()
+        self.ui.tableWidget.insertRow(row)
+        self.ui.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem("Budapest"))
+        self.ui.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem("9876"))
+        print("Button wurde geklickt")
+
+    def onCellChanged(self, row, col):
+        print(row)
+        print(col)
+
+window = MainWindow()
+
+#ui_window = Ui_MainWindow()
+#ui_window.setupUi(window)
 window.show()
 sys.exit(app.exec_())
