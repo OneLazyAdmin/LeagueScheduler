dbd52149d55243747c2854083982fd511ca8796b
Tassilo Schneidt <tschneidt@gmx.de>
2022-08-11 21:58:41 +0200

    Cleaned up the code so it's less embarrassing.
    Also hiding the "Get-Schedule" and "Show-Spoilers"-Button now till you actually need it

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import sys
-import time
-
 import requests
-import json
 import datetime
-import timedelta
 import pytz
 from qtpy import QtWidgets
 from ui.mainwindow import Ui_MainWindow
 from PyQt6 import QtGui, QtCore
-from PyQt6.QtGui import QPixmap, QImage, QIcon
-from PyQt6.QtWidgets import QLineEdit
+from PyQt6.QtGui import QPixmap, QIcon
+
 # non-changing variable
 apikey = '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z'
 schedule = []
@@ -19,7 +15,6 @@ leagues_list_only_names = []
 block_names_set = set()
 block_names_list = []
 block_names_dictionary = {}
-#upcoming_week = ""
 league_id = ""
 app = QtWidgets.QApplication(sys.argv)
 
@@ -31,6 +26,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowTitle("Scheduler")
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.get_schedule.hide()
+        self.ui.show_spoilers.hide()
         self.ui.get_schedule.clicked.connect(self.get_schedule)
         self.ui.timezone.addItems(pytz.all_timezones)
         self.ui.get_timeoffset.clicked.connect(self.calculate_timezone)
@@ -44,16 +41,10 @@ class MainWindow(QtWidgets.QMainWindow):
         headers = {'hl': 'en-US', 'x-api-key': apikey}
         response = (requests.get(url, headers=headers, data=payload))
 
-
-        print(response.content)
         data = response.json()
-        print(data)
         my_leagues = data["data"]["leagues"]
 
         for league in my_leagues:
-            print(league["id"])
-            print(league["name"])
-            print(league["image"])
             temp = {"id": league["id"], "name": league["name"], "image": league["image"]}
             leagues_list.append(temp)
             leagues_list_only_names.append(league["name"])
@@ -63,6 +54,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.ui.chosen_league_combo.currentTextChanged.connect(self.get_block)
         return leagues_list
+
     def get_block(self):
         block_names_set.clear()
         block_names_list.clear()
@@ -80,56 +72,42 @@ class MainWindow(QtWidgets.QMainWindow):
         upcoming_week_found = False
 
         for n_event in my_events:
-            print(n_event)
             while not upcoming_week_found:
                 if n_event["state"] == "unstarted":
                     upcoming_week = n_event["blockName"]
-                    upcoming_week_date = datetime.datetime.strptime((n_event["startTime"].split("T")[0].replace("-", "/")),"%Y/%m/%d").strftime("%d.%m.%Y")
+                    upcoming_week_date = datetime.datetime.strptime(
+                        (n_event["startTime"].split("T")[0].replace("-", "/")), "%Y/%m/%d").strftime("%d.%m.%Y")
                     upcoming_week_string = upcoming_week + ", " + upcoming_week_date
-                    print(upcoming_week_string)
                     upcoming_week_found = True
-                    print("found it")
                 else:
-                    print("didnt find it")
                     break
-            print("Loop done")
-            temp_date = datetime.datetime.strptime((n_event["startTime"].split("T")[0].replace("-", "/")),"%Y/%m/%d").strftime("%d.%m.%Y")
-            temp_block_string = str(n_event["blockName"]) + ", " + str(temp_date)
-            print(temp_block_string)
-            t = time.mktime(datetime.datetime.strptime(temp_block_string.split(", ")[1], "%d.%m.%Y").timetuple())
-            print(t)
+            temp_date = datetime.datetime.strptime((n_event["startTime"].split("T")[0].replace("-", "/")),
+                                                   "%Y/%m/%d").strftime("%d.%m.%Y")
             block_names_set.add((str(n_event["blockName"]) + ", " + str(temp_date)))
-
-        print(block_names_set)
-        print(block_names_list)
-        self.ui.chosen_block.addItems(sorted(block_names_set, key=lambda x: datetime.datetime.strptime(x.split(", ")[1],
-                                                                                        "%d.%m.%Y").timetuple()))
-
+        self.ui.chosen_block.addItems(sorted(block_names_set, key=lambda x: datetime.datetime.strptime
+        (x.split(", ")[1], "%d.%m.%Y").timetuple()))
 
         if upcoming_week_found:
             upcoming_week_index = self.ui.chosen_block.findText(upcoming_week_string, QtCore.Qt.MatchFlag.MatchContains)
             self.ui.chosen_block.setCurrentIndex(upcoming_week_index)
-            self.ui.chosen_block.setItemText(upcoming_week_index, str(self.ui.chosen_block.currentText()) + " (upcoming block)")
-
-        #self.ui.chosen_block.lineEdit()
-        #print(upcoming_week_found)
-        return data
-
+            self.ui.chosen_block.setItemText(upcoming_week_index,
+                                             str(self.ui.chosen_block.currentText()) + " (upcoming block)")
+        self.ui.get_schedule.show()
     def calculate_timezone(self):
         timezone_offset = datetime.datetime.now(
             pytz.timezone(self.ui.timezone.currentText())).utcoffset().total_seconds() / 60 / 60
-        print(timezone_offset)
         return timezone_offset
 
     def calculate_day_of_week(self, start_date):
         day_of_week = start_date.strftime("%A")
         return day_of_week
+
     def show_spoilers(self):
         self.ui.schedule_table.showColumn(3)
         self.ui.schedule_table.showColumn(4)
+
     def get_schedule(self):
         pixmap = QPixmap("Nami.png")
-        print(pixmap)
         self.ui.picture_label.setPixmap(pixmap)
         self.ui.picture_label.show()
         schedule.clear()
@@ -145,39 +123,24 @@ class MainWindow(QtWidgets.QMainWindow):
         data = response.json()
         my_events = data["data"]["schedule"]["events"]
         chosen_block = self.ui.chosen_block.currentText()
-        print(chosen_block)
-        schedule_pretty = json.dumps(data, indent=4)
-        schedule_file = json.dumps(data)
-        print(data)
         self.ui.timezone_title.pixmap()
         for n_event in my_events:
             if n_event["blockName"] in chosen_block:
-                print(n_event)
-                # start_time = n_event["startTime"].split("T")[1].split("Z")[0]
-                # print(start_time)
                 start_timestamp = datetime.datetime.strptime((n_event["startTime"].replace("T", " ").split("Z")[0]),
                                                              "%Y-%m-%d %H:%M:%S")
                 timezone_offset = datetime.timedelta(hours=self.calculate_timezone())
                 start_timestamp = start_timestamp + timezone_offset
                 start_time = start_timestamp.time()
-                print(start_time)
                 start_date = datetime.datetime.strptime((n_event["startTime"].split("T")[0].replace("-", "/")),
                                                         "%Y/%m/%d").strftime("%d.%m.%Y")
                 day_of_week = self.calculate_day_of_week(start_timestamp)
-
-                print(start_date)
                 week = n_event["blockName"]
-                #print(week)
-                # print(n_event["match"]["teams"])
                 team1 = n_event["match"]["teams"][0]["name"]
-                print(team1)
                 team2 = n_event["match"]["teams"][1]["name"]
-                print(team2)
                 if n_event["state"] == "unstarted":
                     result = "TBD"
                 else:
                     result = n_event["match"]["teams"][0]["result"]["outcome"]
-                print(result)
                 if result == "win":
                     winner = team1
                     f = open('image.png', 'wb')
@@ -204,37 +167,30 @@ class MainWindow(QtWidgets.QMainWindow):
                     icon = QIcon(pixmap)
                     icon_table = QtWidgets.QTableWidgetItem()
                     icon_table.setIcon(icon)
-                print(winner)
                 match = team1 + " vs. " + team2
-                print(match)
                 temp = {"startDate": day_of_week + ", " + start_date,
-                        "startTime": str(start_time) + " (UTC + " + str(timezone_offset) + ")", "match": match, \
+                        "startTime": str(start_time) + " (UTC + " + str(timezone_offset) + ")", "match": match,
                         "winner": winner, "week": week, "picture": icon_table}
-                print(temp)
-                print(str(temp["startDate"]).split(", ")[1])
                 if str(temp["startDate"]).split(", ")[1] in chosen_block:
                     schedule.append(temp)
                 else:
                     pass
-
-        print(schedule)
-        print(schedule[0])
         counter = 0
         for item in schedule:
             table_rows = self.ui.schedule_table.rowCount()
-            print(table_rows)
             self.ui.schedule_table.insertRow(table_rows)
             self.ui.schedule_table.setItem(counter, 0, QtWidgets.QTableWidgetItem(item["startDate"]))
             self.ui.schedule_table.setItem(counter, 1, QtWidgets.QTableWidgetItem(item["startTime"]))
             self.ui.schedule_table.setItem(counter, 2, QtWidgets.QTableWidgetItem(item["match"]))
             self.ui.schedule_table.setItem(counter, 3, QtWidgets.QTableWidgetItem(item["winner"]))
             self.ui.schedule_table.setItem(counter, 4, QtWidgets.QTableWidgetItem(item["picture"]))
-            self.ui.schedule_table.item(counter, 4).setBackground(QtGui.QColor(0,100,0))
+            self.ui.schedule_table.item(counter, 4).setBackground(QtGui.QColor(0, 100, 0))
             self.ui.schedule_table.hideColumn(3)
             self.ui.schedule_table.hideColumn(4)
             counter = counter + 1
         self.ui.schedule_table.resizeColumnsToContents()
-        print(block_names_list)
+        self.ui.show_spoilers.show()
+
 
 window = MainWindow()
 window.show()
